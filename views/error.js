6600af694ef260f4a79cdd7fb25e01e5e9c5bad5
PreethiTapati <118854537+PreethiTapati@users.noreply.github.com>
2023-10-13 16:44:01 +0200

    Create error.js

---
+++ views/error.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Not found</title>
+</head>
+<body>
+    <div class="bg">
+        <img class="err-bg" src="/assets/404.jpg" alt="404">
+    <p class="err-message"><%= message %></p></div>
+</body>
+</html>
